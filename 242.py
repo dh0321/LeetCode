1be902a526298c7a39f6590550a9df576e967e02
dh0321 <111106711+dh0321@users.noreply.github.com>
2025-08-04 17:22:34 +0900

    Create 242.py
    242. Valid Anagram
    Hash Table, String, Sorting

---
+++ 242.py
@@ -0,0 +1,69 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for c in count:
+            if c != 0:
+                return False
+        return True
+
+        # return all(c == 0 for c in count)
+
+# ord() returns ascii value of character
+
+'''
+Solution 1.
+def isAnagram(self, s, t):
+    if len(s) != len(t):
+        return False
+    
+    count_s = [0] * 26
+    count_t = [0] * 26
+    
+    for i in range(len(s)):
+        count_s[ord(s[i]) - ord('a')] += 1
+        count_t[ord(t[i]) - ord('a')] += 1
+    
+    return count_s == count_t
+    
+    # from collections import Counter
+    # return Counter(s) == Counter(t)
+
+
+Solution 2.
+def isAnagram(self, s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+
+    for char in s:
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+
+    for char in t:
+        if char not in count:
+            return False
+        count[char] -= 1
+        if count[char] < 0:
+            return False
+
+    return True
+
+
+'''
