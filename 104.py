a5117b0ae9cc62a2cefcc6ade2f9207fde1362cf
dh0321 <111106711+dh0321@users.noreply.github.com>
2025-08-27 17:48:25 +0900

    Create 104.py
    104. Maximum Depth of Binary Tree
    Tree, DFS

---
+++ 104.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        hleft = self.maxDepth(root.left)
+        hright = self.maxDepth(root.right)
+
+        return max(hleft, hright) + 1
+
+'''
+Solution 1.
+
+def maxDepth(self, root):
+    """
+    :type root: Optional[TreeNode]
+    :rtype: int
+    """
+    if not root:
+        return 0
+
+    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+'''
