ef3d5f965f97fec2c63785f79aefd7b2e2dd4491
dh0321 <111106711+dh0321@users.noreply.github.com>
2025-11-06 12:32:45 +0900

    Update 13.py
    not use reversed string

--- 13.py
+++ 13.py
@@ -31,6 +31,37 @@ class Solution(object):
 '''
 Solution 1.
 
+def romanToInt(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    roman = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+
+    sum = 0
+    for i in range(len(s)-1):
+        curr = s[i]
+        next = s[i+1]
+
+        if roman[curr] < roman[next]:
+            sum -= roman[curr]
+        else:
+            sum += roman[curr]
+
+    sum += roman[s[-1]]
+    return sum
+
+
+Solution 2.
+
 roman = {
     'I': 1,
     'V': 5,
